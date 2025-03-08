b216c15f4793bac78dafc7a2017237c59d63a2b9
DESKTOP-AEN9CQI\bhlee <dmzone75@naver.com>
2025-03-08 16:40:33 +0900

    [FE][VITAMIN-UI][#24] feat: Add templates component

---
+++ frontend/packages/vitamin-ui/src/components/templates/SignIn/BaseSignInTemplate.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { BaseSignInTemplate } from './BaseSignInTemplate';
+import { SuccessfulSubmission, ValidationError } from '@/components/organisms/SignIn/BaseSignIn.stories';
+
+const meta: Meta<typeof BaseSignInTemplate> = {
+  title: 'Templates/SignIn/BaseSignInTemplate',
+  component: BaseSignInTemplate,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: '로그인 페이지 기본 템플릿 - 헤더와 로그인 폼을 포함',
+      },
+    },
+    layout: 'centered',
+  },
+  decorators: [(Story) => <Story />],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    onSubmit: (values) => console.log('Submit values:', values),
+    loading: false,
+  },
+};
+
+/** 성공 상태 */
+// 인터랙션 테스트 케이스
+export const SuccessTemplate: Story = {
+  args: Default.args,
+  play: async ({ context }) => {
+    await SuccessfulSubmission.play?.(context);
+  },
+};
+
+export const ValidationErrorTemplate: Story = {
+  args: Default.args,
+  play: async ({ context }) => {
+    await ValidationError.play?.(context);
+  },
+};
---
+++ frontend/packages/vitamin-ui/src/components/templates/SignIn/BaseSignInTemplate.tsx
@@ -0,0 +1,13 @@
+import { BaseSignIn, BaseSignInProps, SignInHeader } from '@/components';
+import * as S from './index.styled';
+
+export const BaseSignInTemplate = ({ onSubmit, loading }: BaseSignInProps) => {
+  return (
+    <S.SignInContainer>
+      <SignInHeader />
+      <S.SignInWrapper>
+        <BaseSignIn onSubmit={onSubmit} loading={loading} />
+      </S.SignInWrapper>
+    </S.SignInContainer>
+  );
+};
