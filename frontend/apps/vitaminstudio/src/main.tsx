63a0ce3c98a8c2a45964455507e64f90a6a0612e
thanksLee <dmzone75@naver.com>
2025-03-08 20:39:38 +0900

    [FE][VITAMINSTUDIO][#25] feat: Set up react configuration. (#27)
    * [FE][VITAMIN-UI][#24] chore: Add alias configuration
    
    * [FE][VITAMIN-UI][#24] chore: Add react package
    
    * [FE][VITAMIN-UI][#24] chore: Add react package
    
    * [FE][VITAMIN-UI][#24] feat: Add atomic component
    
    * [FE][VITAMIN-UI][#24] chore: Add ignore file
    
    * [FE][VITAMIN-UI][#24] feat: Add atomic component
    
    * [FE][VITAMIN-UI][#24] feat: Add components
    
    * [FE][VITAMIN-UI][#24] feat: Add templates component
    
    * [FE][VITAMIN-UI][#24] fix: Lint issue
    
    * [FE][VITAMIN-UI][#24] fix: Ailas bug
    
    * [FE][VITAMINSTUDIO][#25] feat: Set up react configuration.

--- frontend/apps/vitaminstudio/src/main.tsx
+++ frontend/apps/vitaminstudio/src/main.tsx
@@ -1,9 +1,43 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+// 스타일
+import { StyleProvider, legacyLogicalPropertiesTransformer } from '@ant-design/cssinjs'; // 오래된 브라우저와 호환
+import 'nprogress/nprogress.css';
+import { LocaleProvider } from '@workspace/vitamin-ui';
+import { TanstackQuery } from '@/shared/tanstack-query';
+import '@/shared/styles/global.less';
 import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+async function initialApp() {
+  const rootElement = document.getElementById('root');
+  if (!rootElement) return;
+  const root = createRoot(rootElement);
+
+  if (import.meta.env.MODE === 'mock') {
+    // 'mock' 모드일 때만 worker를 시작합니다.
+    const { worker } = await import('./mocks/browser');
+    await worker.start({
+      onUnhandledRequest: 'warn',
+      serviceWorker: {
+        url: '../public/mockServiceWorker.js', // 명시적으로 경로 지정
+      },
+    });
+  }
+
+  root.render(
+    <StyleProvider hashPriority="high" transformers={[legacyLogicalPropertiesTransformer]}>
+      <TanstackQuery isDev={process.env.NODE_ENV === 'development'}>
+        <LocaleProvider>
+          <App />
+        </LocaleProvider>
+      </TanstackQuery>
+    </StyleProvider>
+  );
+
+  // 로딩 표시 숨기기
+  const firstElement = document.getElementById('first');
+  if (firstElement && firstElement.style?.display !== 'none') {
+    firstElement.style.display = 'none';
+  }
+}
+
+initialApp();
